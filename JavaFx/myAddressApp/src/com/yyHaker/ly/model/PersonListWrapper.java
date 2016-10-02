dc22bb2245e5d9f022b8afe1b70dfd628ecc325f
yy <572176750@qq.com>
2016-10-02 16:37:31 +0800

    A JavaFx Application
    the AddressApp is written by JavaFx

---
+++ JavaFx/myAddressApp/src/com/yyHaker/ly/model/PersonListWrapper.java
@@ -0,0 +1,33 @@
+package com.yyHaker.ly.model;
+
+/**
+ * Helper class to wrap a list of persons. This is used for saving the  list of persons to XML.
+ *
+ * @author yyHaker
+ * @create 2016-10-02-14:18
+ */
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Helper class to wrap a list of persons. This is used for saving the
+ * list of persons to XML.
+ *
+ * @author Marco Jakob
+ */
+@XmlRootElement(name = "persons")
+public class PersonListWrapper {
+
+    private List<Person> persons;
+
+    @XmlElement(name = "person")
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+}
